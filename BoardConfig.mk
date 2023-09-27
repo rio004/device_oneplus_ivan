fd9568f483216a5c60d32c72394d298bf2bea0fc
rio004 <riyon336@gmail.com>
2023-09-27 16:31:00 +0100

    ivan: Specify recovery flags
    Signed-off-by: rio004 <riyon336@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,9 @@ BOARD_HAS_MTK_HARDWARE := true
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6877
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Inherit the proprietary files
 include vendor/oneplus/ivan/BoardConfigVendor.mk
